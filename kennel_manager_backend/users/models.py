6d1076be2a03d76832730a7bcd12037464a4158c
jcrippen01 <justin.crippen@gmail.com>
2025-03-10 14:47:04 -0500

    trying to get auth to work

--- kennel_manager_backend/users/models.py
+++ kennel_manager_backend/users/models.py
@@ -1,8 +1,7 @@
-from django.contrib.auth.models import AbstractUser, Group
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class User(AbstractUser):
-    # Custom fields can be added here if needed
     def __str__(self):
         return self.username
